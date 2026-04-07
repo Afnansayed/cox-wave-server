82bbd692bda0139bee0ab327ae7d2fa5400dd0a9
Afnan Sayed <afnansayed1973@gmail.com>
2026-04-07 12:13:53 +0600

    zod validation

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -3,6 +3,9 @@
 import { NextFunction, Request, Response } from "express";
 import { envVars } from "../config/env";
 import status from "http-status";
+import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
+import z from "zod";
+import { handleZodError } from "../errorHelpers/handleZodError";
 
 
 export const globalErrorHandler = (err: any, req: Request, res: Response , _next: NextFunction) => {
@@ -10,12 +13,24 @@ export const globalErrorHandler = (err: any, req: Request, res: Response , _next
         console.error("Error from global error handler: ", err);
     }
      
-    const statusCode: number = status.INTERNAL_SERVER_ERROR;
-    const message: string = "Internal server error";
+    let errorSources:TErrorSources[] = [];
+    let statusCode: number = status.INTERNAL_SERVER_ERROR;
+    let message: string = "Internal server error";
 
-    res.status(statusCode).json({
+    if(err instanceof z.ZodError){
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError.statusCode as number;
+        message = simplifiedError.message;
+        errorSources = [...simplifiedError.errorSources];
+    }
+
+    const errorResponse:TErrorResponse = {
+        statusCode,
         success: false,
         message,
-        error: err.message,
-    })
+        errorSources,
+        error: envVars.NODE_ENV === "development" ? err.message : undefined,
+    }
+
+    res.status(statusCode).json(errorResponse);
 }
--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import { validateRequest } from "../../middleware/validateRequest";
+import { createCustomerZodSchema } from "./auth.validation";
 
 
 const router = Router();
 
-router.post('/register', authController.registerCustomer);
+router.post('/register', validateRequest(createCustomerZodSchema), authController.registerCustomer);
 router.post('/login', authController.loginUser);
 
 export const authRoutes = router;
